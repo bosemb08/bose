360fae620d5b3f233ee507942765515698184ebe
bosemb08 <bosemb08@gmail.com>
2019-09-20 22:17:25 +0530

    dgvg

---
+++ org/test/Day3.java
@@ -0,0 +1,16 @@
+package org.test;
+public class Day3 {
+		public static void main(String[] args) {
+		int count=1;
+		for(int i =1;i<=5;i++)
+		{
+			count=count*i;
+		}
+		
+		System.out.println("the factorial of given number is");
+		System.out.println(count);
+		
+		
+
+			}
+}
